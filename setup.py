c855388a55c01ffcb7e21dd5f314110050688abc
Gibran Mena <gibran.mena@gmail.com>
2025-08-11 15:31:10 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lighthouse_ice',
+    version='0.1.0',
+    author='Gibran Mena-Aguilar',
+    description='Documentation for ICE data analysis',
+    python_requires='>=3',
+    license='GNU General Public License v3',
+    packages=find_packages(),
+)
